d8f28504bdd6da03954da07c3fedb0b4db3f7e7d
Kavya Santhakumar <kavysan@Kavysan-MacBook-Pro-2.local>
2023-12-18 19:53:11 -0500

    added server.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import routes from './config/routes';
 import Navbar from './components/Navbar';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
     <HashRouter>
       <Navbar />
-        <Routes>
-            { routes.map((route: any, index: any) => (
-              <Route
-                key = {index}
-                path = {route.path}
-                element= {
-                  <route.component />
-                }
-                />
-            ))}
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+              { routes.map((route: any, index: any) => (
+                <Route
+                  key = {index}
+                  path = {route.path}
+                  element= {
+                    <route.component />
+                  }
+                  />
+              ))}
+          </Routes>
+        </Provider>
     </HashRouter>
   );
 }
--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,74 @@
+// const token = '41d8904829f74ac97a3694791d970f473ceb305ef2c6b9a7'
+const token = 'c4eef02dd94f3904ba89c35656e81b220aa9a3a64bb62d7c'
+
 export const server_calls = {
-    get: async () => {
+    get: async () => { 
         const response = await fetch(`https://phonebook-vk68.onrender.com/api/contacts`,
         {
             method: 'GET',
-            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            }
+
         });
 
-        if (!response.ok) {
+        if (!response.ok){
             throw new Error('Failed to fetch data from the server')
         }
+
         return await response.json()
-    }
 
-    // finsh other delete update and rest
+        },
 
+        create: async(data: any = {}) => {
+            const response = await fetch(`https://phonebook-vk68.onrender.com/api/contacts`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': `Bearer ${token}`
+                },
+                body: JSON.stringify(data)
+            });
+    
+            if(!response.ok){
+                throw new Error('Failed to Create new data on server')
+            }
+    
+            return await response.json()
+    },
+        update: async (id:string, data:any = {}) => {
+            const response = await fetch(`https://phonebook-vk68.onrender.com/api/contacts${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+                
+            },
+            body: JSON.stringify(data)
+        })
+            if (!response.ok) {
+                throw new Error('Failed to update data on server')
+            }
+
+            return await response.json()
+        },
+
+        delete: async(id:string) => {
+            const response = await fetch(`https://phonebook-vk68.onrender.com/api/contacts${id}`,{
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'x-access-token': `Bearer ${token}`
+                },
+    
+            })
+        if (!response.ok) {
+            throw new Error('Failed to delete data on server')
+        }
+
+        return;
+    },
 }
